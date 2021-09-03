bbdadfd0a6efadd55bb85e5ad0015c6dfbfc6b77
biswat <biswajit.18.dash@gmail.com>
2021-09-03 20:37:44 +0530

    just me

--- my-app/src/files/projects.js
+++ my-app/src/files/projects.js
@@ -42,29 +42,21 @@ export class projects extends React.Component {
         return (
             <div>
             <h1>PROJECTS</h1>
-                <div>
-                <ul className="project" onMouseMove = {this._onMouseMove} ref="project" style={maskStyle}>
+                <div className="project pro">
+                <ul className="pro-det" onMouseMove = {this._onMouseMove} ref="project" style={maskStyle}>
 
-                <li>
-                <div className="pro" style={{fontSize: '1.2vw'}}> <a style={{fontSize: '2vw'}} href="https://biswat.github.io/med_pharm/">MED PHARM</a> <br />
+                <li className="">
+                <a  style={{fontSize: '2vw'}} href="https://biswat.github.io/med_pharm/">MED PHARM</a> <br />
                 a medical e-commerce site, which have a aim to provide a better service as compired to other such examples
-                </div>
                 </li>
 
-                <li className="two">
-                <div className="pro" style={{fontSize: '1.2vw'}}> <a style={{fontSize: '2vw'}} href="https://github.com/biswat/protein_analysis">PROTEIN ANALYSIS</a> <br />
+                <li className="">
+                <a style={{fontSize: '2vw'}} href="https://github.com/biswat/protein_analysis">PROTEIN ANALYSIS</a> <br />
                 a protein analysis tool which gives a details analysis report on a protein sequence, like that of other protein analysis tool
-                </div>
                 </li>
-
-                <li>
-                <div><h1>{text}</h1></div>
-                </li>
-
                 </ul>
-                
                 </div>
-                <img className="project" src={work} alt="" />
+                <img className="project img" src={work} alt="" />
             </div>
         )
     }
