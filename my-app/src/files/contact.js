69286b99238ea8dc61888c23ba558f5979ced904
biswat <biswajit.18.dash@gmail.com>
2021-09-04 23:06:46 +0530

    just me

--- my-app/src/files/contact.js
+++ my-app/src/files/contact.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import '../App.css'
+import gmail from '../assests/images/gmail.svg'
+import linkedin from '../assests/images/linkedin.svg'
+import instagram from '../assests/images/instagram.svg'
+import whatsapp from '../assests/images/whatsapp.svg'
+import github from '../assests/images/github.svg'
 
 export class contact extends Component {
     static propTypes = {
@@ -12,7 +17,14 @@ export class contact extends Component {
             <div>
             <h1>CONTACT ME</h1>
                 <div className="contact">
-                    <h5></h5>
+                    <b>You can contact me by : -</b>
+                    <ul className="cont">
+                    <li><a href=""><img src={github} alt="" /></a></li>
+                    <li><a href=""><img src={gmail} alt="" /></a></li>
+                    <li><a href=""><img src={linkedin} alt="" /></a></li>
+                    <li><a href=""><img src={instagram} alt="" /></a></li>
+                    <li><a href=""><img src={whatsapp} alt="" /></a></li>
+                    </ul>
                 </div>
             </div>
         )
